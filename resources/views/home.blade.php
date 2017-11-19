d9a372acecc72276858152c66fddc0b5b99fe330
sloba82 <dizajnistampa@gmail.com>
2017-11-19 15:20:02 +0100

    tiddied

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,69 @@
 @extends('layouts.app')
 
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!-- Top content -->
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Dashboard</div>
 
-                    You are logged in!
-                </div>
+            <div class="panel-body">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                You are logged in!
             </div>
         </div>
     </div>
 </div>
+
+
+<div class="top-content">
+    <div class="inner-bg">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 text">
+                    <div class="logo wow fadeInDown">
+                        <a href="index.html"></a>
+                    </div>
+                    <h1 class="wow fadeInLeftBig">Auto servis Ljuba ms</h1>
+
+                    <div class="description wow fadeInLeftBig">
+                        <p>
+                            ovde neki tekst
+                        </p>
+                    </div>
+                    <div class="top-big-link wow fadeInUp">
+                        <a class="btn btn-link-1" href="#">Zakaži servis</a>
+                        <a class="btn btn-link-2" href="#services">Proveri status vozila</a>
+                    </div>
+                </div>
+            </div>
+         </div>
+    </div>
+</div>
+<div id="services">
+    <div class="container-fluid" style="background-color: #f4f4f4; ">
+        <img src="{{URL::asset('app/assets/img/body.jpg')}}">
+    </div>
+</div>
+
+<!-- Footer -->
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+
+            logo
+            <div class="col-sm-12 footer-bottom">
+                <a class="scroll-link" href="#top-content"><i class="fa fa-chevron-up"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
