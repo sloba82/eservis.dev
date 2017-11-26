c112b1db61846efa06e147a13ba2fb0f832c5ca9
sloba82 <dizajnistampa@gmail.com>
2017-11-26 14:08:13 +0100

    template_setup_finished
    first step in temlapatin index an hom page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,79 @@
 
 @extends('layouts.head')
 
+
+@section('nav')
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a href="#" class="navbar-left">
+                    <img src="{{ URL::asset('app/assets/img/logo2.png')}}">
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="top-navbar-1">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a  href="#services">Services</a></li>
+                    <li><a  href="#portfolio">Work</a></li>
+                    <li><a  href="#testimonials">Clients</a></li>
+                    <li><a  href="#footer">Contact</a></li>
+
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+@endsection
+
+
+
 @section('body')
-            @yield('content')
+
+
+
+
+
+@yield('content')
 
 
         <!-- Scripts -->
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -56,6 +56,7 @@
     </div>
 </div>
 
+
 <!-- Footer -->
 <footer>
     <div class="container-fluid">
