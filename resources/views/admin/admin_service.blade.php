b9c019f51d1414847771c4c9f2d38473440adc5b
sloba82 <dizajnistampa@gmail.com>
2018-08-21 16:18:56 +0200

    fix

--- resources/views/admin/admin_service.blade.php
+++ resources/views/admin/admin_service.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-   {{-- <div class="container">
+<div class="container">
 
         <div class="row">
 
@@ -52,143 +52,8 @@
 
        </div>
 
-    </div>--}}
+</div>
 
-
-
-   <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-   <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-   <!------ Include the above in your HEAD tag ---------->
-
-   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
-   <!--
-       instagram: www.instagram.com/programmingtutorial
-       site: programlamadersleri.net
-   -->
-
-   <div class="box">
-       <div class="container">
-           <div class="row">
-
-               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-
-                   <div class="box-part text-center">
-
-                       <i class="fa fa-behance fa-3x" aria-hidden="true"></i>
-
-                       <div class="title">
-                           <h3>Behance</h3>
-                       </div>
-
-                       <div class="text">
-                           <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                       </div>
-
-                       <a href="#">Learn More</a>
-
-                   </div>
-               </div>
-
-               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-
-                   <div class="box-part text-center">
-
-                       <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
-
-                       <div class="title">
-                           <h3>Twitter</h3>
-                       </div>
-
-                       <div class="text">
-                           <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                       </div>
-
-                       <a href="#">Learn More</a>
-
-                   </div>
-               </div>
-
-               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-
-                   <div class="box-part text-center">
-
-                       <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
-
-                       <div class="title">
-                           <h3>Facebook</h3>
-                       </div>
-
-                       <div class="text">
-                           <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                       </div>
-
-                       <a href="#">Learn More</a>
-
-                   </div>
-               </div>
-
-               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-
-                   <div class="box-part text-center">
-
-                       <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
-
-                       <div class="title">
-                           <h3>Pinterest</h3>
-                       </div>
-
-                       <div class="text">
-                           <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                       </div>
-
-                       <a href="#">Learn More</a>
-
-                   </div>
-               </div>
-
-               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-
-                   <div class="box-part text-center">
-
-                       <i class="fa fa-google-plus fa-3x" aria-hidden="true"></i>
-
-                       <div class="title">
-                           <h3>Google</h3>
-                       </div>
-
-                       <div class="text">
-                           <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                       </div>
-
-                       <a href="#">Learn More</a>
-
-                   </div>
-               </div>
-
-               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-
-                   <div class="box-part text-center">
-
-                       <i class="fa fa-github fa-3x" aria-hidden="true"></i>
-
-                       <div class="title">
-                           <h3>Github</h3>
-                       </div>
-
-                       <div class="text">
-                           <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
-                       </div>
-
-                       <a href="#">Learn More</a>
-
-                   </div>
-               </div>
-
-           </div>
-       </div>
-   </div>
 
 
 
