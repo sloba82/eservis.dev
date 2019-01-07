4cc46b3b10072650fb5195c5a769fcd47e0f74d0
sloba82 <dizajnistampa@gmail.com>
2019-01-07 16:55:13 +0100

    removed car user tablefrom projest it is no longer nesesery

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -14,11 +14,42 @@ class UsersSeeder extends Seeder
     {
 
 
+
+
         DB::table('users')->insert([
 
             'name' => 'admin',
+            'last_name' => 'admin',
             'email' => 'admin@admin.com',
-            'password' => md5('admin'),
+            'password' => bcrypt('admin'),
+            'address' => 'Petra Petrovica 6',
+            'city' => 'Novi Sad',
+            'phone' => '0642465848',
+            'role' => 1,
+            'created_at' => '2018-02-18 01:20:44',
+            'updated_at' => '2018-02-18 01:20:44',
+        ]);
+
+        DB::table('users')->insert([
+
+            'name' => 'serviceman',
+            'last_name' => 'service',
+            'email' => 'serviceman@serviceman.com',
+            'password' => bcrypt('serviceman'),
+            'address' => 'Petra Petrovica 6',
+            'city' => 'Novi Sad',
+            'phone' => '0642465848',
+            'role' => 3,
+            'created_at' => '2018-02-18 01:20:44',
+            'updated_at' => '2018-02-18 01:20:44',
+        ]);
+
+        DB::table('users')->insert([
+
+            'name' => 'user',
+            'last_name' => 'user',
+            'email' => 'user@user.com',
+            'password' => bcrypt('user'),
             'address' => 'Petra Petrovica 6',
             'city' => 'Novi Sad',
             'phone' => '0642465848',
